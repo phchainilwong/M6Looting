c7e976e981de2d3bbc56e109a6545320f1a27444
phchainilwong <phchainilwong@s.sfusd.edu>
2026-02-03 11:45:03 -0800

    M6L4 & M6L5

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func open() -> void:
+	animation_player.play("open")
+	input_pickable = false
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
@@ -17,3 +22,13 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
